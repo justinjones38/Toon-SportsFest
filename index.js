d615ef09127b9874f349ab48c747856f64c8f41a
Justin <justinjones38@myyahoo.com>
2025-11-06 23:50:53 -0600

    Styling changes and adding interactivity

--- index.js
+++ index.js
@@ -10,13 +10,22 @@
 
 // Step 1: Select the theme button
 let themeButton = document.getElementById("theme-button");
-
+let buttonSymbol = document.getElementById("button-symbol");
+let lightMode = true;
 
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
     // Write your code here
     // This section will run whenever the button is clicked
     document.body.classList.toggle("dark-mode");
+    lightMode = !lightMode;
+    if (lightMode) {
+      buttonSymbol.src = `./img/dark_mode.png`;
+    } else {
+      buttonSymbol.src = `./img/light_mode.png`;
+    }
+
+
 }
 
 
@@ -25,8 +34,125 @@ const toggleDarkMode = () => {
 themeButton.addEventListener("click", toggleDarkMode);
 
 
+// Handling expectations button
 
-/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+// Step 1: grabbing button and expectation-info 
+let expectationButton = document.getElementById("expectations-button");
+let expectationsInfo = document.getElementsByClassName("expectations-info")[0];
+
+// Step 2: Adding function to enable and disable event expectations
+const showExpectations = () => {
+  if (expectationsInfo.style.display == "none" || expectationsInfo.style.display == "") {
+    expectationsInfo.style.display = "flex";
+    expectationButton.textContent = "Hide Expectations"
+  } else {
+    expectationsInfo.style.display = "none";
+    expectationButton.textContent = "Show Expectations"
+  }
+}
+
+// Step 3: Adding event Listener to make expectations appear/disappear
+expectationButton.addEventListener("click", showExpectations);
+
+
+
+// Handling Featured events button
+
+// Step 1: getting primary button class
+let primaryEvent = document.getElementsByClassName("primary-events-container")[0];
+let featuredEvents = document.getElementById("featured-events");
+
+// Step 2: primary event button
+const handlePrimaryEvent = () => {
+  if (primaryEvent.style.display == "none" || primaryEvent.style.display == "") {
+    primaryEvent.style.display = "block";
+    featuredEvents.textContent = "Hide Featured Events";
+  } else {
+    primaryEvent.style.display = "none";
+    featuredEvents.textContent = "Show Featured Events"
+  }
+}
+
+// Step 3: add Event Listener 
+featuredEvents.addEventListener("click", handlePrimaryEvent);
+
+
+
+/*** Form Handling ***
+  
+  Purpose:
+  - When the user submits the RSVP form, the name and state they 
+    entered should be added to the list of participants.
+
+  When To Modify:
+  - [ ] Project 6 (REQUIRED FEATURE)
+  - [ ] Project 6 (STRETCH FEATURE) 
+  - [ ] Project 7 (REQUIRED FEATURE)
+  - [ ] Project 9 (REQUIRED FEATURE)
+  - [ ] Any time between / after
+***/
+
+// Adding the view button to display the other events
+
+  // Step 1: Get view more button
+  let viewButton = document.getElementById("view-button");
+
+  // Step 2: Get extra events container 
+  let extraEvents = document.getElementById("extra-events");
+
+  // Step 3: Create function to display and hide events
+  const addEventsToDoc = () => {
+    if (extraEvents.style.display == "" || extraEvents.style.display == "none") {
+      extraEvents.style.display = "grid";
+      viewButton.textContent = "Hide Events";
+      console.log("Input logged");
+    } else {
+      extraEvents.style.display = "none";
+      viewButton.textContent = "View More";
+    }
+  }
+
+  // Add event listener to listen for click
+  viewButton.addEventListener("click", addEventsToDoc);
+
+
+// Step 1: Add your query for the submit RSVP button here
+let button = document.getElementById("rsvp-button");
+let count = 3;
+const addParticipant = (event) => {
+    event.preventDefault();
+    // Step 2: Write your code to manipulate the DOM here
+    let name = document.getElementById("name").value;
+    let state = document.getElementById("state").value;
+    let email = document.getElementById("email").value;
+
+    const p = document.createElement("p");
+    p.textContent = `${name} from ${state} has RSVP'd`;
+    let rsvpParticipants = document.getElementsByClassName("rsvp-participants")[0];
+    rsvpParticipants.appendChild(p);
+
+    // grabbing rsvp count id and removing from DOM
+    let rsvpCount = document.getElementById("rsvp-count");
+    rsvpCount.remove();
+
+    // Updating count
+    count += 1;
+
+    // creating new counter element
+    const newRsvpCount = document.createElement('p');
+    newRsvpCount.setAttribute("id", "rsvp-count");
+    newRsvpCount.textContent = `⭐ ${count} people have RSVP'd to this event!`;
+
+    // appending to DOM
+    rsvpParticipants.appendChild(newRsvpCount);
+
+
+
+    
+}
+
+// Step 3: Add a click event listener to the submit RSVP button here
+button.addEventListener("click", addParticipant);
 /*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
 /*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
 /*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
